a92a5240c034f6a46e281cb167da8433fc5e7a6d
RoccoDev <hey@rocco.dev>
2021-01-07 10:53:14 +0100

    cargo: add files

---
+++ build.rs
@@ -0,0 +1,23 @@
+// Copyright (C) 2021 RoccoDev
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+fn main() {
+    protobuf_codegen_pure::Codegen::new()
+        .out_dir("src/protos")
+        .inputs(&["protos/gamelog_core.proto", "protos/gamelog_cai.proto"])
+        .include("protos")
+        .run()
+        .expect("Codegen failed.");
+}
