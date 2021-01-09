2c65de77ad1dc141b7e6c5c197ac396d1266f323
RoccoDev <hey@rocco.dev>
2021-01-09 18:25:43 +0100

    web: dark mode

---
+++ static/js/theme.js
@@ -0,0 +1,17 @@
+const locals = JSON.parse(localStorage.getItem("kig-theme"))
+const stylesheet = document.getElementById("bootstrap-dark")
+const toggler = document.getElementById("theme-toggler")
+
+setDarkTheme(locals && locals.dark)
+
+function toggleDarkTheme() {
+    setDarkTheme(!darkTheme)
+}
+
+function setDarkTheme(dark) {
+    darkTheme = dark
+    stylesheet.setAttribute("href", dark ? "/static/css/bootstrap-dark.min.css" : "")
+    document.body.setAttribute("data-theme", dark ? "dark" : "") // For other CSS files
+    localStorage.setItem("kig-theme", JSON.stringify({ dark }))
+    toggler.innerHTML = `Theme: ${dark ? "Dark" : "Light"}`
+}
